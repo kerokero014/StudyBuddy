71403b0b517a2092a29df38c9f171cb873f037ea
kerokero014 <kevinjrm.kr@gmail.com>
2024-05-28 20:48:07 -0600

    pages

---
+++ studybuddy/src/app/courses/page.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/MainHeader";
+import Footer from "../components/MainFooter";
+
+export default function Courses() {
+  return (
+    <>
+      <Header />
+
+      <h1>Courses</h1>
+
+      <Footer />
+    </>
+  );
+}
